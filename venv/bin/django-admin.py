2f29d8d4c05e74a6396f4bf914621e5cf1d329b6
bandris342 <andras.bartok@gmail.com>
2018-01-17 16:47:03 +0100

    First commit

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/bandris/.local/share/Trash/files/CoursPy/CoursPy/agil/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

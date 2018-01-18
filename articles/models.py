841b8f3ac58cc2c45cff46374e32b655846f5841
bandris342 <andras.bartok@gmail.com>
2018-01-18 13:44:15 +0100

    Add app template

--- articles/models.py
+++ articles/models.py
@@ -4,3 +4,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+
+    titre = models.CharField(max_length=255)
+    description = models.TextField()
+    author = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.titre
--- articles/views.py
+++ articles/views.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from .models import Article
+
 # Create your views here.
+
+def first_view(request):
+
+    articles = Article.objects.all
+    context = {'articles': articles}
+    return render(request, 'articles/index.html', context)
